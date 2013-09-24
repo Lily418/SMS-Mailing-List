bf77c8ac2bf5ad9566edc19f63e2939b54cd2723
Lily Hoskin <lilyhoskin@gmail.com>
2013-09-24 17:55:08 +0100

    Enabled Debugging

--- index.py
+++ index.py
@@ -5,14 +5,24 @@ from socketio import socketio_manage
 from socketio.namespace import BaseNamespace
 from werkzeug.wsgi import SharedDataMiddleware
 from socketio.server import SocketIOServer
+from werkzeug.debug import DebuggedApplication
+from gevent import monkey
+
+monkey.patch_all()
 
 class VoteNamespace(BaseNamespace):
     def onVote(self, msg):
         self.emit("vote", msg);
 
 app = Flask("app18297361")
+app.debug = True
 app.config['MONGO_URI'] = os.getenv("MONGOHQ_URL", "mongodb://localhost:27017/sms-mailing-list")
 mongo = PyMongo(app)
+
+
+@app.route("/broken")
+def beBroken():
+    pass
 
 @app.route("/graph")
 def showGraph():
@@ -37,8 +47,7 @@ def index():
     return response
 
 if __name__ == '__main__':
-    app = SharedDataMiddleware(app, {'/': os.path.join(os.path.dirname(__file__), 'static')})
-    app.debug = True  
+    app = DebuggedApplication(app, evalex=True)
     global socketIOServer
     socketIOServer = SocketIOServer(('0.0.0.0', 8080), app, namespace="socket.io", policy_server=False)
     print('Listening on http://localhost:8080')
