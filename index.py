b73b493632218aec2a527e433a77a3389b661a84
Lily Hoskin <lilyhoskin@gmail.com>
2013-09-24 12:54:05 +0100

    Allowed POST requests

--- index.py
+++ index.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
     response = make_response(render_template("Response.xml"))
     response.headers['Content-Type'] = "text/xml"
