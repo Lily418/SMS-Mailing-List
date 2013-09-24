d763b81da5f3586cbadfb086084abf38a70e6ad1
Lily Hoskin <lilyhoskin@gmail.com>
2013-09-24 13:57:13 +0100

    App should save text msg in mongodb

--- index.py
+++ index.py
@@ -3,13 +3,13 @@ from flask import Flask, render_template, make_response, request
 from flask.ext.pymongo import PyMongo
 
 app = Flask("app18297361")
-#app.config['MONGO_URI'] = os.getenv("MONGOHQ_URL", "mongodb://localhost:27017/sms-mailing-list")
-#mongo = PyMongo(app)
+app.config['MONGO_URI'] = os.getenv("MONGOHQ_URL", "mongodb://localhost:27017/sms-mailing-list")
+mongo = PyMongo(app)
 
 
 @app.route('/', methods=['GET','POST'])
 def index():
-    #mongo.db.messages.insert(request.args)
+    mongo.db.messages.insert(request.args)
     response = make_response(render_template("Response.xml"))
     response.headers['Content-Type'] = "text/xml"
     return response
