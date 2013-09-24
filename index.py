f1be2cb54d080123fa152166349c41af4e63376a
Lily Hoskin <lilyhoskin@gmail.com>
2013-09-24 12:39:06 +0100

    Added response for Twilio

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    response = make_response(render_template("Response.xml"))
+    response.headers['content-type'] = "text/xml"
+    return render_template("Response.xml")
 
