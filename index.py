a6bfe0ccd18059bdc2c972d12bede0496f3cab9b
Lily Hoskin <lilyhoskin@gmail.com>
2013-09-24 13:14:09 +0100

    Save request args to mongodb

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
 import os
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, request
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = os.getenv("MONGOLAB_URL", "mongodb://localhost:27017/sms-mailing-list")
+mongo = PyMongo(app)
+
 
 @app.route('/', methods=['GET','POST'])
 def index():
+    mongo.db.messages.insert(request.args)
     response = make_response(render_template("Response.xml"))
     response.headers['Content-Type'] = "text/xml"
     return response
